6cb82f7540c586307b21d3242d4c1450669707f2
nevitonvelho <nevitonvelho01@gmail.com>
2024-10-20 21:16:13 -0300

    teset

---
+++ app/src/main/java/com/example/milkgestor/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.milkgestor
+
+import android.os.Bundle
+import android.provider.SyncStateContract.Columns
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.milkgestor.ui.theme.MilkGestorTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+
+import androidx.compose.material3.Icon
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MilkGestorTheme {
+                Hero()
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun Hero() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 32.dp, start = 16.dp, end = 16.dp, bottom = 16.dp), // Adiciona espaçamento no topo
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // Box no lado esquerdo com texto estilizado
+        Box {
+            Text(
+                text = "MilkGestor",
+                fontSize = 24.sp, // Tamanho maior do texto
+                fontWeight = FontWeight.Bold // Texto em negrito
+            )
+        }
+
+        // Box no lado direito com avatar pequeno
+        Box {
+            Image(
+                painter = painterResource(id = R.drawable._720741804176), // Substitua com o recurso do avatar correto
+                contentDescription = "Avatar",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+            )
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MilkGestorTheme {
+
+    }
+}
